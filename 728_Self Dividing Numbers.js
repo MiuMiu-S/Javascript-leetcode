a9271467308da2b32b743c7144e1cdd1d50e5b02
MiuMiu_S <3290555376@qq.com>
2018-03-22 17:58:18 +0800

    Create 728_Self Dividing Numbers.js
    easy

---
+++ 728_Self Dividing Numbers.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDivide = function(num) {
+	let arry = num.toString().split('')
+	console.log(num);
+	for (let j = 0; j < arry.length; j++) {
+		if (num % arry[j] !== 0) {
+			return false
+		}
+	}
+	return true;
+}
+
+var selfDividingNumbers = function(left, right) {
+	let num = new Array();　
+	for (let i = left; i <= right; i++) {
+		console.log("-----------" + i);
+		if (i % 10 == 0) continue;
+		if (selfDivide(i)) {
+			num.push(i)
+		}
+
+	}
+	return num;
+};
+console.log(selfDividingNumbers(47, 85))

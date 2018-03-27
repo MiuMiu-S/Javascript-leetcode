47f648f3b93fa280b0402da7d6d158a8c0f1a829
MiuMiu_S <3290555376@qq.com>
2018-03-27 11:10:31 +0800

    easy

--- 557. Reverse Words in a String III_easy.js
+++ 557. Reverse Words in a String III_easy.js
@@ -1,7 +1,7 @@
 var reverseWords = function(s) {
-	let a = s.split(" ")
-	for (let m = 0; m < a.length; m++) {
-		a[m] = a[m].split("").reverse().join("")
+	let b = s.split(" ")
+	for (let m = 0; m < b.length; m++) {
+		b[m] = b[m].split("").reverse().join("")
 	}
-	return a.join(" ")
+	return b.join(" ")
 };

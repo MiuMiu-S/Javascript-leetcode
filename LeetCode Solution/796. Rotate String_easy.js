eee96ddc0d0e0bf628a626e25dc4ede82dc514fd
MiuMiu_S <3290555376@qq.com>
2018-08-02 13:31:24 +0800

    easy

--- LeetCode Solution/796. Rotate String_easy.js
+++
@@ -1,9 +0,0 @@
-/**
- * 796. Rotate String
- * @param {string} A
- * @param {string} B
- * @return {boolean}
- */
-var rotateString = function(A, B) {
-	return A.length == B.length && ((A + A).indexOf(B) != -1);
-};

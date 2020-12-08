302450ad12b413af67836a7d36d4456ec49bdd5e
shf <3290555376@qq.com>
2020-12-08 10:02:46 +0800

    20201208

---
+++ LeetCode Solution/1281. 整数的各位积和之差-easy.js
@@ -0,0 +1,41 @@
+// 2020
+// 1281. 整数的各位积和之差
+// 给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+
+// 示例 1：
+// 输入：n = 234
+// 输出：15 
+// 解释：
+// 各位数之积 = 2 * 3 * 4 = 24 
+// 各位数之和 = 2 + 3 + 4 = 9 
+// 结果 = 24 - 9 = 15
+
+// 示例 2：
+// 输入：n = 4421
+// 输出：21
+// 解释： 
+// 各位数之积 = 4 * 4 * 2 * 1 = 32 
+// 各位数之和 = 4 + 4 + 2 + 1 = 11 
+// 结果 = 32 - 11 = 21
+
+// 提示：
+// 1 <= n <= 10^5
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    const arr = n.toString().split('');
+    const product = arr.reduce((prev, cur) => ( prev * cur ), 1);
+    const sum = arr.reduce((prev, cur) => ( +cur + prev ), 0);
+    return product - sum;
+};
+console.log(1, subtractProductAndSum(234));
+
+// var subtractProductAndSum = function(n) {
+//     let arr = n.toString().split('');
+//     let a = eval(arr.join('*'));
+//     let b = eval(arr.join('+'));
+//     return a-b;
+// };
